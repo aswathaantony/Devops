aeea9dcbfafc2e049a8b39c401617ddb4aea789b
aswathaantony <98990716+aswathaantony@users.noreply.github.com>
2024-04-18 18:29:21 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-        return "Hello world! ECS deployment"
+        return "Hello all! ECS deployment"
 
 
 app.run(host='0.0.0.0',port=8000)
